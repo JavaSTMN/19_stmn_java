94ff61b292bef29ee12dd0558246517c4ea5c930
Thomas Cianfarani <thomas.ci83@gmail.com>
2019-09-04 16:38:28 +0200

    Création du projet Eclipse et ajout de tests

---
+++ JavaBasics1/src/FirstProgramPackage/FirstProgram.java
@@ -0,0 +1,17 @@
+package FirstProgramPackage;
+
+public class FirstProgram {
+	
+	public static double moy(double[] values) throws Exception {
+		if (values.length == 0) 
+			throw new Exception("Division par 0!");
+		
+		double sum = 0;
+		for(double value : values) {
+			sum += value;
+		}
+		
+		return sum / values.length;
+	} 
+	
+}
---
+++ JavaBasics1/src/FirstProgramPackage/FirstProgramTest.java
@@ -0,0 +1,19 @@
+package FirstProgramPackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstProgramTest {
+
+	@Test
+	void moyTestNominal() throws Exception {
+		assertEquals(1.5f, FirstProgram.moy(new double[] { 1, 2 }));
+	}
+	
+	@Test
+	void moyTestThrow() throws Exception {
+		assertEquals(1.5f, FirstProgram.moy(new double[] { 1, 2 }));
+	}
+
+}
