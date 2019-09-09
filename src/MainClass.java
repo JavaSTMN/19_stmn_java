c6bb87fefb74bf25701fc820fd50ae3a320acf53
Paul Michels <paul_michels@hotmail.com>
2019-09-09 11:04:39 +0200

    Exercice 1

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,23 +1,44 @@
 public class MainClass {
 	
 	public static void main(String[] args) {
-		try {
-			System.out.println("Moyenne : " + moy(new double[] { 1.0, 12.0, 85.0, 56.0, 140.0}));
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+
 	}
 
-	public static double moy(double[] values) throws Exception{
-		if(values.length > 0) {
-			double sum = 0;
-			for(double value:values) {
-				sum += value;
-			}
-			return sum/values.length;
-		} else {
-			throw new Exception("Aucune valeur");
+	public static double moy(double[] values) throws Exception {
+		if(values.length == 0) {
+			throw new Exception("No value.");
 		}
+
+		double sum = 0;
+		for(double value:values) {
+			sum += value;
+		}
+		return sum/values.length;
+	}
+	
+	public static int[][] multiply(int[][] A, int[][] B) throws Exception{
+
+		if(A == null){
+			throw new Exception("Matrix A is null.");
+		}
+
+		if(B == null){
+			throw new Exception("Matrix B is null.");
+		}
+
+		if(A[0].length != B.length){
+			throw new Exception("Matrix's dimensions mismatch.");
+		}
+
+		int[][] C = new int[A.length][B[0].length];
+		for(int i = 0; i < A.length; i++) {
+			for(int j = 0; j < B[0].length; j++) {
+				for (int k = 0; k < B.length; k++){
+					C[i][j] += A[i][k] * B[k][j];
+				}
+			}
+		}
+		return C;
 	}
 	
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,12 +1,59 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class MainClassTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void moyNominal() throws Exception{
+		assertEquals(2.0, MainClass.moy(new double[]{1.0, 3.0}));
 	}
 
+	@Test
+	void moyFail() throws Exception{
+		assertNotEquals(1.0, MainClass.moy(new double[]{1.0, 3.0}));
+	}
+	
+	@Test
+	void moyException(){
+		assertThrows(Exception.class, ()->{MainClass.moy(new double[] {});});
+	}
+
+	@Test
+	void multiplyNominal() throws Exception{
+		int[][] A = new int[][]{{1, 2, 3},{4, 5, 6}};
+		int[][] B = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+		int[][] C = new int[][]{{38, 44, 50, 56}, {83, 98, 113, 128}};
+		assertArrayEquals(C, MainClass.multiply(A, B));
+	}
+
+	@Test
+	void multiplyFail() throws Exception{
+		int[][] A = new int[][]{{1, 2, 3},{4, 5, 6}};
+		int[][] B = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+		int[][] C = new int[][]{{3, 5, 8, 0}, {7, 8, 4, 5}};
+		assertNotEquals(C, MainClass.multiply(A, B));
+	}
+
+	@Test
+	void multiplyDimensionsMismatch(){
+		int[][] A = new int[2][2];
+		int[][] B = new int[3][3];
+		assertThrows(Exception.class, ()->MainClass.multiply(A, B));
+	}
+
+	@Test
+	void multiplyNullMatrixA(){
+		int[][] A = new int[][]{{1, 2, 3},{4, 5, 6}};
+		int[][] B = null;
+		assertThrows(Exception.class, ()->{MainClass.multiply(A, B);});
+	}
+
+	@Test
+	void multiplyNullMatrixB(){
+		int[][] A = null;
+		int[][] B = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+		assertThrows(Exception.class, ()->{MainClass.multiply(A, B);});
+	}
 }
